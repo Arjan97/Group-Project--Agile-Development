9c3325a193fede41536a63e28ac9d3484c041809
Wessel <rigterwessel@gmail.com>
2022-03-18 15:18:17 +0100

    Adds Template project

---
+++ Game1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace BaseProject
+{
+    public class Game1 : GameEnvironment
+    {      
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+
+            screen = new Point(800, 600);
+            ApplyResolutionSettings();
+
+            // TODO: use this.Content to load your game content here
+
+        }
+
+    }
+}
