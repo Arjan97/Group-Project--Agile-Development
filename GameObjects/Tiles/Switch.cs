7fc6706ce8db80ffc148c7416a073df1dddea5e9
Wessel <rigterwessel@gmail.com>
2022-03-24 17:17:41 +0100

    Adds switch trap activation function

--- GameObjects/Tiles/Switch.cs
+++ GameObjects/Tiles/Switch.cs
@@ -8,8 +8,17 @@ namespace BaseProject.GameObjects.Tiles
     {
         public Switch(int x, int y, int length, int x2, int y2, int length2) : base(x, y)
         {
-            Add(new SwitchObject(x, y, length));
-            Add(new SwitchObject(x2, y2, length2));
+            Add(new SwitchObject(x, y, length, 1));
+            Add(new SwitchObject(x2, y2, length2, 2));
+        }
+
+        public void Activate(string choise)
+        {
+            Activated = true;
+
+            //searches chosen trap and activates it
+            SwitchObject target = (SwitchObject)Find(choise);
+            target.Activate();
         }
     }
 }
