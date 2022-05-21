c724981fe9f1ae8db5f9c05a968b13ee3f6358e1
Arjan <arjan.peeters@hva.nl>
2022-05-21 20:56:02 +0200

    a

--- GameObjects/Player.cs
+++ GameObjects/Player.cs
@@ -13,7 +13,7 @@ namespace BaseProject.GameObjects
         public bool isFalling, isColliding, keyPressed, isGrounded, isJumping, jumpKeyPressed, died, blockMovement, facingLeft;
         public Vector2 pVelocity;
         public string verticalCollidingSide;
-        public int jumpframes, blockedframes, lives, maxLives =3;
+        public int jumpframes, blockedframes, lives, maxLives = 3;
         private float timer;
         public bool isDashing;
         private float dashDuration;
@@ -87,11 +87,11 @@ namespace BaseProject.GameObjects
 
         public override void Update(GameTime gameTime)
         {
-            
+
             float i = 1;
 
             //dying when falling of the map
-            if(position.Y > GameEnvironment.Screen.Y)
+            if (position.Y > GameEnvironment.Screen.Y)
             {
                 death();
             }
@@ -109,7 +109,7 @@ namespace BaseProject.GameObjects
                 }
                 else
                 {
-                   // velocity.Y -= 6;
+                    // velocity.Y -= 6;
                 }
                 PlayAnimation("jump");
                 jumpframes++;
@@ -119,21 +119,21 @@ namespace BaseProject.GameObjects
                 jumpframes = 1;
                 isJumping = false;
             }
-            
+
             //adding gravity
             if (!isGrounded)
             {
                 velocity.Y += 4.5f * i;
             }
 
-            if(blockMovement)
+            if (blockMovement)
             {
                 blockedframes++;
             }
 
             velocity *= 70f;
 
-            
+
             {
 
             }
@@ -163,7 +163,7 @@ namespace BaseProject.GameObjects
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            for(int i=0; i<lives; i++)
+            for (int i = 0; i < lives; i++)
             {
                 livesIcons.Children[i].Draw(gameTime, spriteBatch);
             }
@@ -173,245 +173,257 @@ namespace BaseProject.GameObjects
         public override void HandleInput(InputHelper inputHelper)
         {
             //Player push ability
-            if (inputHelper.IsKeyDown(input.Player(Buttons.Y))){
+            if (inputHelper.IsKeyDown(input.Player(Buttons.Y)))
+            {
 
-                if(!PushCooldown)
+                if (!PushCooldown)
                 {
                     PushCooldown = true;
                     PushObject.Scale = new Vector2(2, 2);
                     PushObject.Position = position - new Vector2(0, sprite.Height / 2);
                     PushObject.Visible = true;
 
-                    if(sprite.Mirror)
+                    if (sprite.Mirror)
                     {
                         PushObject.Velocity = new Vector2(-PushSpeed, 0);
-                    } else
+                    }
+                    else
                     {
                         PushObject.Velocity = new Vector2(PushSpeed, 0);
                     }
-                } else
+                }
+                else
                 {
 
-            //Player Dash ability
-            if (inputHelper.IsKeyDown(Keys.LeftShift))
-            {
-                isDashing = true;
-                dashDuration++;
+                    //Player Dash ability
+                    if (inputHelper.IsKeyDown(input.Player(Buttons.Y)))
+                    {
+                        isDashing = true;
+                        dashDuration++;
 
-                if (dashDuration <= 10 && !isFacingLeft)
-                {
-                    velocity.X += dashPower;
+                        if (dashDuration <= 10 && !isFacingLeft)
+                        {
+                            velocity.X += dashPower;
+
+                        }
+                        else if (dashDuration <= 10 && isFacingLeft)
+                        {
+                            velocity.X += -dashPower;
+                        }
+                    }
+                    //Checks if the player is dashing, then a cooldown is issued
+                    if (isDashing)
+                    {
+                        timer++;
+                        if (timer >= 200)
+                        {
+                            dashDuration = 0;
+                            timer = 0;
+                            isDashing = false;
+                        }
+                    }
+
+                    //code to block all movement except the dash
+                    if (blockMovement)
+                    {
+                        if (blockedframes == 15)
+                        {
+                            blockMovement = false;
+                            blockedframes = 0;
+                        }
+                        //possible 'stunned' animation
+                        PlayAnimation("idle");
+                        return;
+                    }
+
+                    base.HandleInput(inputHelper);
+
+                    if (inputHelper.IsKeyDown(input.Player(Buttons.left)))
+                    {
+                        velocity.X += -speed;
+                        PlayAnimation("run");
+                        facingLeft = true;
+                        isFacingLeft = true;
+                    }
+
+                    else if (inputHelper.IsKeyDown(input.Player(Buttons.right)))
+                    {
+                        velocity.X += speed;
+                        isFacingLeft = false;
+                        facingLeft = false;
+                        PlayAnimation("run");
+
+                    }
+                    else
+                    {
+                        PlayAnimation("idle");
+                    }
+
+                    if (!inputHelper.IsKeyDown(input.Player(Buttons.left)))
+                    {
+                        isFacingLeft = false;
+                    }
+
+
+                    if (inputHelper.IsKeyDown(input.Player(Buttons.up)) || inputHelper.IsKeyDown(input.Player(Buttons.B))  /* && isGrounded */)
+                    {
+                        isColliding = false;
+                        keyPressed = true;
+                        isJumping = true;
+                        jumpKeyPressed = true;
+                    }
+                    else if (!inputHelper.IsKeyDown(input.Player(Buttons.up)) && !inputHelper.IsKeyDown(input.Player(Buttons.B)))
+                    {
+                        jumpKeyPressed = false;
+                    }
+
+                    sprite.Mirror = facingLeft;
 
                 }
-                else if (dashDuration <= 10 && isFacingLeft)
-                {
-                    velocity.X += -dashPower;
-                }
             }
-            //Checks if the player is dashing, then a cooldown is issued
-            if (isDashing)
-            {
-                timer++;
-                if (timer >= 200)
-                {
-                    dashDuration = 0;
-                    timer = 0;
-                    isDashing = false;
-                }
-            }
-
-            //code to block all movement except the dash
-            if (blockMovement)
-            {
-                if(blockedframes == 15)
-                {
-                    blockMovement = false;
-                    blockedframes = 0;
-                }
-                //possible 'stunned' animation
-                PlayAnimation("idle");
-                return;
-            }
-
-            base.HandleInput(inputHelper);
-
-            if (inputHelper.IsKeyDown(input.Player(Buttons.left)))
-            {
-                velocity.X += -speed;
-                PlayAnimation("run");
-                facingLeft = true;
-                isFacingLeft = true;
-            }
-
-            else if (inputHelper.IsKeyDown(input.Player(Buttons.right)))
-            {
-                velocity.X += speed;
-                isFacingLeft = false;
-                facingLeft = false;
-                PlayAnimation("run");
-                
-            } else
-            {
-                PlayAnimation("idle");
-            }
-
-            if (!inputHelper.IsKeyDown(input.Player(Buttons.left)))
-            {
-                isFacingLeft = false;
-            }
-
-
-            if (inputHelper.IsKeyDown(input.Player(Buttons.up)) || inputHelper.IsKeyDown(input.Player(Buttons.B))  /* && isGrounded */)
-            {
-                isColliding = false;
-                keyPressed = true;
-                isJumping = true;
-                jumpKeyPressed = true;
-            }
-            else if (!inputHelper.IsKeyDown(input.Player(Buttons.up)) && !inputHelper.IsKeyDown(input.Player(Buttons.B)))
-            {
-                jumpKeyPressed = false;
-            }
-
-            sprite.Mirror = facingLeft;
-
         }
+        
 
-        public void HandleColission(Tile tile)
-        {
-            //checking and handling collision with SpikeTile
-            if (tile is SpikeTile || tile is SpikeRoofTile)
+            public void HandleColission(Tile tile)
             {
-               
-                death();
+                //checking and handling collision with SpikeTile
+                if (tile is SpikeTile || tile is SpikeRoofTile)
+                {
 
-            }
-            //checking and handling collision with SwitchTile
-            if(tile is SwitchTile)
-            {
-              SwitchObject switchTile = (SwitchObject)tile.Parent;
-                if (switchTile.Armed) { 
- 
+                    death();
+
+                }
+                //checking and handling collision with SwitchTile
+                if (tile is SwitchTile)
+                {
+                    SwitchObject switchTile = (SwitchObject)tile.Parent;
+                    if (switchTile.Armed) {
+
                         death();
 
+                    }
                 }
-            }
 
-            //checking and handling collision with FinishTile
-            if(tile is FinishTile)
-            {
-                nextLevel();
-            }
-            
-            Vector2 intersection = Collision.CalculateIntersectionDepth(BoundingBox, tile.BoundingBox);
-
-            //checking if its a vertical collision
-            if (Math.Abs(intersection.X) > Math.Abs(intersection.Y))
-            {
-               
-                //collision bottom side player and top side tile
-                if (intersection.Y < 0)
+                //checking and handling collision with FinishTile
+                if (tile is FinishTile)
                 {
-                    isColliding = true;
-                    isGrounded = true;
-                    verticalCollidingSide = "down";
-                    position.Y -= Math.Abs(intersection.Y) - 1;
-                    
+                    nextLevel();
                 }
-                //collision top side player and bottom side tile
+
+                Vector2 intersection = Collision.CalculateIntersectionDepth(BoundingBox, tile.BoundingBox);
+
+                //checking if its a vertical collision
+                if (Math.Abs(intersection.X) > Math.Abs(intersection.Y))
+                {
+
+                    //collision bottom side player and top side tile
+                    if (intersection.Y < 0)
+                    {
+                        isColliding = true;
+                        isGrounded = true;
+                        verticalCollidingSide = "down";
+                        position.Y -= Math.Abs(intersection.Y) - 1;
+
+                    }
+                    //collision top side player and bottom side tile
+                    else
+                    {
+                        isColliding = true;
+                        verticalCollidingSide = "up";
+
+                    }
+                }
                 else
                 {
-                    isColliding = true;
-                    verticalCollidingSide = "up";
+                    //collision right side player and left side tile
+                    if (intersection.X < 0)
+                    {
+                        isColliding = true;
+                        position.X -= Math.Abs(intersection.X) - 1;
+                    }
+                    //collision left side player and right side tile
+                    else
+                    {
+                        isColliding = true;
+                        position.X += Math.Abs(intersection.X) + 1;
+                    }
 
+                    //blocking movement when player keeps colliding with wall.
+                    if (intersection.X > 8 || intersection.X < -8)
+                    {
+                        blockMovement = true;
+                    }
                 }
             }
-            else
-            {
-                //collision right side player and left side tile
-                if (intersection.X < 0)
+        
+
+                //function thats moves the player, gets called when colliding with push projectile
+                public void getPushed(float speed)
                 {
-                    isColliding = true;
-                    position.X -= Math.Abs(intersection.X) -1;
+                    //checks if the push projectile is moving left or right
+                    if (speed > 0)
+                    {
+                        velocity.X += 30;
+                    }
+                    else
+                    {
+                        velocity.X -= 30;
+                    }
                 }
-                //collision left side player and right side tile
-                else
+
+                //function to handle the death of a player
+                void death()
                 {
-                    isColliding = true;
-                    position.X += Math.Abs(intersection.X) +1;
+                    //TODO death annimation
+                    lives--;
+                    died = true;
+                    if (lives <= 0)//checks if the player can respawn
+                    {
+                        GameEnvironment.GameStateManager.SwitchTo("gameOverState");
+                        return;
+                    }
+                    else
+                    {
+                        PlayingState play = (PlayingState)GameEnvironment.GameStateManager.GetGameState("playingState");
+                        play.tileList.nextLevelNr = play.tileList.currentLevel;
+                        Respawn();
+                        play.ghost.Reset();
+                        //System.Diagnostics.Debug.WriteLine(lives);
+                    }
                 }
-
-                //blocking movement when player keeps colliding with wall.
-                if(intersection.X > 8 || intersection.X < -8)
+                //method to change level
+                void nextLevel()
                 {
-                    blockMovement = true;
+                    PlayingState play = (PlayingState)GameEnvironment.GameStateManager.GetGameState("playingState");
+                    play.tileList.nextLevelNr = play.tileList.currentLevel + 1;
+
+                    Reset();
+                    play.ghost.Reset();
                 }
-            }
-        }
 
-        //function thats moves the player, gets called when colliding with push projectile
-        public void getPushed(float speed)
-        {
-            //checks if the push projectile is moving left or right
-            if(speed > 0)
-            {
-                velocity.X += 30;
-            }
-            else
-            {
-                velocity.X -= 30;
-            }
-        }
+                //function to create the lives icons
+                void createLives()
+                {
+                    livesIcons = new GameObjectList(0, "lives");
+                    livesIcons.Parent = Parent;
+                    livesIcons.Position = new Vector2(30, GameEnvironment.Screen.Y * 1 / 10);
 
-        //function to handle the death of a player
-        void death()
-        {
-            //TODO death annimation
-            lives--;
-            died = true;
-            if(lives <= 0)//checks if the player can respawn
-            {
-                GameEnvironment.GameStateManager.SwitchTo("gameOverState");
-                return;
-            }
-            else
-            {
-            PlayingState play =(PlayingState) GameEnvironment.GameStateManager.GetGameState("playingState");
-            play.tileList.nextLevelNr = play.tileList.currentLevel;
-             Respawn();
-             play.ghost.Reset();
-             //System.Diagnostics.Debug.WriteLine(lives);
-            }
-        }
-        //method to change level
-        void nextLevel()
-        {
-            PlayingState play = (PlayingState)GameEnvironment.GameStateManager.GetGameState("playingState");
-            play.tileList.nextLevelNr = play.tileList.currentLevel +1;
+                    //creates an Icon for each live
+                    for (int i = 0; i < maxLives; i++)
+                    {
+                        SpriteGameObject live = new SpriteGameObject("img/players/spr_testplayer");
+                        live.Scale = new Vector2(0.5f, 0.5f);
+                        live.Position += new Vector2(live.Sprite.Width * i, 0);
+                        livesIcons.Add(live);
+                    }
+                }
+                void SetOriginToBottomCenter()
+                {
+                    Origin = new Vector2(sprite.Width / 2, sprite.Height);
+                }
             
-            Reset();
-            play.ghost.Reset();
-        }
-
-        //function to create the lives icons
-        void createLives()
-        {
-            livesIcons = new GameObjectList(0, "lives");
-            livesIcons.Parent = Parent;
-            livesIcons.Position = new Vector2(30, GameEnvironment.Screen.Y *1/10);
-
-            //creates an Icon for each live
-            for(int i = 0; i< maxLives; i++)
-            {
-                SpriteGameObject live = new SpriteGameObject("img/players/spr_testplayer");
-                live.Scale = new Vector2(0.5f, 0.5f);
-                live.Position += new Vector2(live.Sprite.Width * i,0);
-                livesIcons.Add(live);
-            }
-        }
-        void SetOriginToBottomCenter()
-        {
-            Origin = new Vector2(sprite.Width / 2, sprite.Height);
-        }
+       
     }
 }
+
+
