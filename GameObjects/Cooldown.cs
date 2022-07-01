4c018262af030670b5332bdc494830b6fb10e443
Wessel <rigterwessel@gmail.com>
2022-07-01 14:41:20 +0200

    Adds cooldown class

---
+++ GameObjects/Cooldown.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace BaseProject.GameObjects
+{
+    public class Cooldown : SpriteGameObject
+    {
+        public Cooldown(string assetName, GameObject gameEnvironment, Vector2 position ) : base(assetName)
+        {
+            parent = gameEnvironment;
+            this.position = position;
+        }
+
+        public void Update(int percentage, GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+    }
+}
